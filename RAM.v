d51b46625a76bc6e6357cd3fd95c77b933a6e471
Dino-svg <angel.cortes8356@alumnos.udg.mx>
2025-03-25 15:26:31 -0600

    Update RAM.v

--- RAM.v
+++ RAM.v
@@ -1,12 +1,25 @@
-`timescale 1ns/1ns
-
-module RAM (
-	input En,
-	input [3:0] dirR,
-	input [31:0] dataIn,
-	output reg [31:0] DS
+module mem_out(
+	input  wire[31:0]direccion,
+	input  wire Wen,
+	input  wire Ren,
+	input  wire[31:0] datoEscritura,
+	output reg [31:0] datoLectura
 );
 
-reg [31:0]memRam[0:15];
+reg [31:0]memr_out[0:31];
 
+always@(*)
+	begin
+	//Escritura
+	if(Wen && !Ren) 
+	begin
+		memr_out[direccion] = datoEscritura;
+	end
+	
+	//Lectura
+	if(Ren && !Wen) 
+	begin
+		datoLectura = memr_out[direccion];
+	end
+end
 endmodule
