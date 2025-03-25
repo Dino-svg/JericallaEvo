a2b3cea6c8bc63ea776a845555200851a21e08b0
Dino-svg <angel.cortes8356@alumnos.udg.mx>
2025-03-25 15:42:24 -0600

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,20 +1,20 @@
 module ALU(
-    input wire [31:0] A,
-    input wire [31:0] B,
-    input wire [3:0] ALU_Sel,
-    output reg [31:0] R
+    input wire [31:0] operand_A,
+    input wire [31:0] operand_B,
+    input wire [3:0] operation,
+    output reg [31:0] result
 );
 
 always@(*) 
-	begin
-		case (ALU_Sel)
-			4'b0000: R = A & B;        				// AND
-			4'b0001: R = A | B;        				// OR
-			4'b0010: R = A + B;       				// SUMA
-			4'b0110: R = A - B;        				// RESTA
-			4'b0111: R = (A < B) ? 32'd1 : 32'd0; 	// Ternario A < B	
-			4'b1100: R = ~(A | B);     				// NOR
-			default: R = 32'd0;       				// Por defecto, resultado en 0
-		endcase
-	end
+    begin
+        case (operation)
+            4'b0000: result = operand_A & operand_B;        // AND
+            4'b0001: result = operand_A | operand_B;        // OR
+            4'b0010: result = operand_A + operand_B;        // SUMA
+            4'b0110: result = operand_A - operand_B;        // RESTA
+            4'b0111: result = (operand_A < operand_B) ? 32'd1 : 32'd0; // Comparación
+            4'b1100: result = ~(operand_A | operand_B);     // NOR
+            default: result = 32'd0;                       // Default
+        endcase
+    end
 endmodule
