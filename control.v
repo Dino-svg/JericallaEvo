339678a39893ae257c22f220c7e9685e6c75b111
IvanAlexisGV <ivan.guevara6431@alumnos.udg.mx>
2025-03-25 23:28:52 -0600

    Update control.v

--- control.v
+++ control.v
@@ -1,4 +1,5 @@
 module control(
+	
     input  wire[1:0]  opcode,
     output reg [7:0]  control_signals
 );
@@ -6,12 +7,13 @@ module control(
 always@(*)
     begin
         case(opcode)
-            2'b00: control_signals = 8'b1_0_0_0010_1; // Suma
-            2'b01: control_signals = 8'b1_0_0_0110_1; // Resta
-            2'b10: control_signals = 8'b1_0_0_0111_1; // Comparación
-            2'b11: control_signals = 8'b0_1_1_0000_0; // Store Word
+            2'b00: control_signals = 8'b1_0_0_0010_1; 
+            2'b01: control_signals = 8'b1_0_0_0110_1; 
+            2'b10: control_signals = 8'b1_0_0_0111_1; 
+            2'b11: control_signals = 8'b0_1_1_0000_0; 
         endcase
     end
+	
 endmodule
 
 
