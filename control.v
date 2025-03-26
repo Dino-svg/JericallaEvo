c4bf3707e26630636dcefb09bea4abff6f52fed2
Dino-svg <angel.cortes8356@alumnos.udg.mx>
2025-03-25 21:34:52 -0600

    Update control.v

--- control.v
+++ control.v
@@ -1,17 +1,17 @@
-module control(
-	input  wire[1:0]  opcode,
-	output reg [7:0]  salida_control
+module ControlUnit(
+    input  wire[1:0]  opcode,
+    output reg [7:0]  control_signals
 );
-
+	
 always@(*)
-	begin
-		case(opcode)
-			2'b00: salida_control = 8'b1_0_0_0010_0;		//Suma aritmética.
-			2'b01: salida_control = 8'b1_0_0_0110_0;		//Resta aritmética
-			2'b10: salida_control = 8'b1_0_0_0111_0;		//Operación ternaria
-			2'b11: salida_control = 8'b0_1_1_0000_1;		
-		endcase
-	end
+    begin
+        case(opcode)
+            2'b00: control_signals = 8'b1_0_0_0010_1; // Suma
+            2'b01: control_signals = 8'b1_0_0_0110_1; // Resta
+            2'b10: control_signals = 8'b1_0_0_0111_1; // Comparación
+            2'b11: control_signals = 8'b0_1_1_0000_0; // Store Word
+        endcase
+    end
 endmodule
 
 
