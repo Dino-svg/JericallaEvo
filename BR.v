6475d99bff53605d09489d613b9ed79003f833f0
Dino-svg <angel.cortes8356@alumnos.udg.mx>
2025-03-25 21:31:56 -0600

    Update BR.v

--- BR.v
+++ BR.v
@@ -1,8 +1,8 @@
-module RegisterBank(
+module RegisterFile(
     input  wire[4:0] read_addr1,
     input  wire[4:0] read_addr2,
     input  wire[4:0] write_addr,
-    input  wire reg_write,
+    input  wire write_enable,
     input  wire[31:0] data_in,
     output reg [31:0] data_out1,
     output reg [31:0] data_out2
@@ -12,13 +12,14 @@ reg [31:0] registers[0:31];
 
 always@(*)
     begin
+    // Lectura de registros
     data_out1 = registers[read_addr1];
     data_out2 = registers[read_addr2];
     
-    if(reg_write) 
+    // Escritura de registro
+    if(write_enable) 
     begin
         registers[write_addr] = data_in;
     end
-    
 end
 endmodule
