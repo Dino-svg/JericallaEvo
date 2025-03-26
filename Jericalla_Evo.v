e9292abefbf8e9b0d70681b35c3a92184ec19128
Dino-svg <angel.cortes8356@alumnos.udg.mx>
2025-03-25 21:34:06 -0600

    Update Jericalla_Evo.v

--- Jericalla_Evo.v
+++ Jericalla_Evo.v
@@ -3,50 +3,68 @@
 module JericallaEvo(
     input   [16:0] instruction,
     input    clock,
-    output  [31:0] result_out
+    output  [31:0] output_data
 );
 
-wire [31:0] w1;     
-wire [7:0]  w2;     
-wire [31:0] w3;     
-wire [31:0] w4; 
-wire [31:0] w5;     
-wire [31:0] w6;     
-wire [31:0] w7;     
-wire [31:0] w8;     
-wire [31:0] w9;     
-wire [31:0] w10;    
-wire [31:0] w11;    
-wire [31:0] w12;    
+// Conexiones entre módulos
+wire [31:0] write_data;      // Dato escritura RegisterFile y salida_B PipelineReg2
+wire [7:0]  control_signals; // Señales de control
+wire [31:0] read_data1;      // Dato lectura 1 RegisterFile
+wire [31:0] read_data2;      // Dato lectura 2 RegisterFile
+wire [31:0] mem_address;     // Dirección memoria
+wire [31:0] alu_operandA;    // Operando A ALU
+wire [31:0] alu_operandB;    // Operando B ALU
+wire [31:0] alu_result;      // Resultado ALU
+wire [31:0] demux_out1;      // Salida demux
+wire [31:0] mem_write_data;  // Dato escritura memoria
+wire [31:0] pipe1_outA;      // Salida A PipelineReg1
+wire [31:0] pipe1_outC;      // Salida C PipelineReg1 (feedback)
 
-RegisterBank      register_bank(.read_addr1(instruction[9:5]), .read_addr2(instruction[4:0]), .write_addr(instruction[14:10]), .reg_write(w2[0]), .data_in(w1), .data_out1(w3), .data_out2(w4));
-memory_output     memory_unit(.address(w5), .write_en(w2[6]), .read_en(w2[7]), .write_data(w10), .read_data(result_out));
-ALU alu_unit(.operand_A(w6), .operand_B(w7), .operation(w2[4:1]), .result(w8));
-demux             demux_unit(.entrada_demux(w11), .demux_sel(w2[5]), .salida_demux_1(w6), .salida_demux_2(w9));
-PipelineRegister  pipe_reg1(.input1(w3), .input2(w4), .input3(w12), .clock(clock), .output1(w11), .output2(w7), .output3(w12));
-PipelineRegister  pipe_reg2(.input1(w9), .input2(w8), .input3(w7), .clock(clock), .output1(w5), .output2(w1), .output3(w10));
-control           control_unit(.opcode(instruction[16:15]), .salida_control(w2));
+// Instancias
+RegisterFile     reg_file(.read_addr1(instruction[9:5]), .read_addr2(instruction[4:0]), 
+                         .write_addr(instruction[14:10]), .write_enable(control_signals[0]), 
+                         .data_in(write_data), .data_out1(read_data1), .data_out2(read_data2));
+MemoryUnit       memory(.address(mem_address), .write_enable(control_signals[6]), 
+                       .read_enable(control_signals[7]), .data_in(mem_write_data), 
+                       .data_out(output_data));
+ALU   alu(.operand_A(alu_operandA), .operand_B(alu_operandB), 
+                     .operation(control_signals[4:1]), .result(alu_result));
+demux            demux(.entrada_demux(pipe1_outA), .demux_sel(control_signals[5]), 
+                       .salida_demux_1(alu_operandA), .salida_demux_2(demux_out1));
+Buffer pipe_reg1(.input_A(read_data1), .input_B(read_data2), .input_C(pipe1_outC), 
+                           .clock(clock), .output_A(pipe1_outA), .output_B(alu_operandB), 
+                           .output_C(pipe1_outC));
+PipelineRegister pipe_reg2(.input_A(demux_out1), .input_B(alu_result), .input_C(alu_operandB), 
+                           .clock(clock), .output_A(mem_address), .output_B(write_data), 
+                           .output_C(mem_write_data));
+control          control_unit(.opcode(instruction[16:15]), .salida_control(control_signals));
 endmodule
 
 module JericallaEvo_TB();
 
 reg   [16:0] instruction;
 reg   clock;
-wire  [31:0] result_out;
+wire  [31:0] output_data;
 
-JericallaEvo dut(.instruction(instruction), .clock(clock), .result_out(result_out));
+JericallaEvo dut(.instruction(instruction), .clock(clock), .output_data(output_data));
 
 // Testbench
-always #25 clock=~clock;
+always #25 clk=~clk;
 initial
     begin
         clock=0;
-        $readmemb("datos.txt", dut.register_bank.registers);
+        $readmemb("datos.txt", dut.reg_file.registers);
         instruction = 17'b00_00100_00000_00001;    // Suma aritmética
         #100;
         instruction = 17'b01_00101_00001_00010;    // Resta aritmética
         #100;
         instruction = 17'b10_00110_00010_00011;    // Operación ternaria
+        #100;
+        instruction = 17'b11_00000_00111_00100;    // SW
+        #100;
+        instruction = 17'b11_00000_01000_00101;    // SW
+        #100;
+        instruction = 17'b11_00000_01001_00110;    // SW
         #100;
         $stop;
     end
